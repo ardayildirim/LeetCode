574db92ecc231e6c379ef05efd98349d6907924c
Arda Abdullah YILDIRIM <57501234+ardayildirim@users.noreply.github.com>
2020-05-03 13:15:26 +0300

    Update 2-Jewels_and_Stones.cpp

--- 1.week/2-Jewels_and_Stones.cpp
+++ 1.week/2-Jewels_and_Stones.cpp
@@ -2,9 +2,8 @@ int numJewelsInStones(string J, string S)
     {
         int ans = 0;
         char *p = &S[0];
-        for(char c : J)
+        for(char c : J) // For every character in J we count the number of occurences in S
         {
-            
             for(p = &S[0];*p;p++)
             {
                 if(c == *p) ans++;
