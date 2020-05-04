c8a825ca8df95e5717c8c6067a76585a91b91fda
Arda Abdullah YILDIRIM <57501234+ardayildirim@users.noreply.github.com>
2020-05-04 15:41:28 +0300

    Recursive sol

---
+++ 1.week/3-Ransom_Note.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        def rec(ran,mag):
+            if ran == "":
+                return True;
+            if mag.count(ran[0]) == 0:
+                return False
+            x = mag.find(ran[0])
+            mag = mag[:x] + mag[x+1:]
+            return rec(ran[1:],mag)
+        return rec(ransomNote,magazine)
